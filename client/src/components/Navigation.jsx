fcf7efe0b56e93c8754fc423abb19e2173138ca0
Sam-Hoeterickx <125275911+Sam-Hoeterickx@users.noreply.github.com>
2025-04-03 16:54:11 +0200

    Refactor Countdown component styles, add logo SVGs, and enhance footer layout

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router';
 
-import Image from '../../public/logo-placeholder-image.png'
+import Image from '../../public/logo.svg'
 
 //Components
 import Button from './Button.jsx';
@@ -9,12 +9,14 @@ const Navigation = () => {
 
     return(
         <nav>
+            <div className="inner-wrapper">
                 <Link to='/' className='home-link'><img src={Image} alt="logo event" /></Link>
                 <Button
                     destinationUrl={'/register'}
                     copy={"Inschrijven"}
                     className={"button"}
                 />
+            </div>
         </nav>
     )
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,9 +11,12 @@ document.title = "Expo 2025 "
     return (
         <div>
             <section className="hero-wrapper">
-                <div className="hero-inner-wrapper">
-                    <h2>Expo 2025&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;20 juni&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;Anderlecht </h2>
+                <div className="inner-wrapper hero">
+                    <h2 id="quick-info">Expo 2025&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;20 juni&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;Anderlecht </h2>
                     <CountDown />
+                    <p>
+                        Kom de afstudeerprojecten van onze derdejaarsstudenten ontdekken tijdens dit unieke expo/festival. Laat je inspireren door innovatief design, technologie en creativiteit. Schrijf je vandaag nog in en verzeker jezelf van een plekje op dit bijzondere event.
+                    </p>
                     <Button
                         destinationUrl={'/register'}
                         copy={"Inschrijven"}
@@ -21,65 +24,80 @@ document.title = "Expo 2025 "
                     />
                 </div>
             </section>
-            <section className="info">
-                <p>
-                    Kom de afstudeerprojecten van onze derdejaarsstudenten ontdekken tijdens dit unieke expo/festival. Laat je inspireren door innovatief design, technologie en creativiteit. Schrijf je vandaag nog in en verzeker jezelf van een plekje op dit bijzondere event.
-                </p>
-            </section>
             <section className="practical-info-wrapper">
-                <h2>Praktische info</h2>
-                <p>
-                    Welkom bij [naam evenement], dé expo waar innovatie, design en creativiteit samenkomen! Dit unieke festival biedt een podium aan de afstudeerprojecten van onze derdejaarsstudenten, waarbij baanbrekende ideeën en vernieuwende technologieën worden gepresenteerd. Daarnaast worden ook de beste werken van studenten uit alle jaren tentoongesteld.
-                    <br /><br />
-                    Het evenement is niet alleen een viering van talent, maar ook een inspiratiemoment voor toekomstige studenten. Overweeg je een studie in deze richting? Dan is dit de perfecte kans om de sfeer te proeven, in gesprek te gaan met studenten en docenten, en te ontdekken wat onze opleiding te bieden heeft.
-                    <br /><br />
-                    Bovendien zullen vertegenwoordigers van bedrijven aanwezig zijn, wat dit event ook een uitstekende netwerkmogelijkheid maakt voor iedereen die geïnteresseerd is in de creatieve en technologische sector.
-                    <br /><br />
-                    Mis het niet—schrijf je in en laat je inspireren!
-                </p>
-                <div className="practical-info-innerwrapper">
-                    <div className="specific-info-wrapper">
-                        <InfoLine 
-                            icon="./"
-                            copy="20 juni 2025"
-                        />
-                        <InfoLine 
-                            icon="./"
-                            copy="Vanaf 16 uur"
-                        />
-                        <InfoLine 
-                            icon="./"
-                            copy="Gratis maar inschrijven verplicht"
-                        />
-                        <InfoLine 
-                            icon="./"
-                            copy="Nijverheidskaai 170, 1070 Anderlecht"
-                        />
+                <div className="inner-wrapper">
+                    <h2>Praktische info</h2>
+                    <p>
+                        Welkom bij [naam evenement], dé expo waar innovatie, design en creativiteit samenkomen! Dit unieke festival biedt een podium aan de afstudeerprojecten van onze derdejaarsstudenten, waarbij baanbrekende ideeën en vernieuwende technologieën worden gepresenteerd. Daarnaast worden ook de beste werken van studenten uit alle jaren tentoongesteld.
+                        <br /><br />
+                        Het evenement is niet alleen een viering van talent, maar ook een inspiratiemoment voor toekomstige studenten. Overweeg je een studie in deze richting? Dan is dit de perfecte kans om de sfeer te proeven, in gesprek te gaan met studenten en docenten, en te ontdekken wat onze opleiding te bieden heeft.
+                        <br /><br />
+                        Bovendien zullen vertegenwoordigers van bedrijven aanwezig zijn, wat dit event ook een uitstekende netwerkmogelijkheid maakt voor iedereen die geïnteresseerd is in de creatieve en technologische sector.
+                        <br /><br />
+                        Mis het niet—schrijf je in en laat je inspireren!
+                    </p>
+                    <div className="practical-info-inner-wrapper">
+                        {/* <div className="specific-info-wrapper">
+                            <InfoLine 
+                                icon="./"
+                                copy="20 juni 2025"
+                            />
+                            <InfoLine 
+                                icon="./"
+                                copy="Vanaf 16 uur"
+                            />
+                            <InfoLine 
+                                icon="./"
+                                copy="Gratis maar inschrijven verplicht"
+                            />
+                            <InfoLine 
+                                icon="./"
+                                copy="Nijverheidskaai 170, 1070 Anderlecht"
+                            />
+                        </div> */}
+                        <div className="specific-info">
+                            <div className="specific-info-upper-wrapper">
+                                <h3>20 juni 2025</h3>
+                                <h3>16u - 21u</h3>
+                                <Button 
+                                    destinationUrl={"register"}
+                                    copy={"Inschrijven"}
+                                    className={"button"}
+                                />
+                            </div>
+                            <div className="specific-info-lower-wrapper">
+                                <p>Gratis, maar inschrijven verplicht</p>
+                                <p>Nijverheidskaai 170, 1070 Brussel
+                                <br />Campus Kaai</p>
+                            </div>
+                        </div>
+                        <div className="google-maps-wrapper">
+                            <iframe 
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.4754936891018!2d4.322295361603742!3d50.84161158071!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c40f19faf0f9%3A0x6a19083b298c5df0!2sNijverheidskaai%20170%2C%201070%20Anderlecht!5e0!3m2!1snl!2sbe!4v1743596139302!5m2!1snl!2sbe" 
+                                width="600" 
+                                height="450" 
+                                style={{ border: 0 }} 
+                                allowFullScreen="" 
+                                loading="lazy" 
+                                referrerPolicy="no-referrer-when-downgrade"
+                            ></iframe>
+                        </div>
                     </div>
-                    <div className="google-maps-wrapper">
-                        <iframe 
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.4754936891018!2d4.322295361603742!3d50.84161158071!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c40f19faf0f9%3A0x6a19083b298c5df0!2sNijverheidskaai%20170%2C%201070%20Anderlecht!5e0!3m2!1snl!2sbe!4v1743596139302!5m2!1snl!2sbe" 
-                            width="600" 
-                            height="450" 
-                            style={{ border: 0 }} 
-                            allowFullScreen="" 
-                            loading="lazy" 
-                            referrerPolicy="no-referrer-when-downgrade"
-                        ></iframe>
-                    </div>
+                
                 </div>
-                <Button 
-                    destinationUrl={"register"}
-                    copy={"Inschrijven"}
-                    className={"button"}
-                />
             </section>
             <section className="study-area-wrapper">
-                <h2>Multimedia en creatieve technologie</h2>
-                <p>
-                    Multimedia & Creatieve Technologie is een veelzijdige studierichting waarin technologie en creativiteit samenkomen. Studenten leren interactieve en digitale ervaringen ontwerpen, ontwikkelen en realiseren met tools zoals webdesign, animatie, AR/VR, videoproductie en programmeren. De focus ligt op innovatie, storytelling en technische vaardigheden, waardoor afgestudeerden klaar zijn voor een carrière in de creatieve en digitale sector.
-                </p>
-                <a href="https://www.erasmushogeschool.be/nl/opleidingen/multimedia-en-creatieve-technologie" target="_blank" rel="noopener noreferrer">Meer</a>
+                <div className="inner-wrapper study-area">
+                    <img src="/homepage-image.png" alt="" />
+                    <div className="study-area-info-inner-wrapper">
+                        <h2>Multimedia en creatieve technologie</h2>
+                        <p>
+                            Multimedia & Creatieve Technologie is een veelzijdige studierichting waarin technologie en creativiteit samenkomen. Studenten leren interactieve en digitale ervaringen ontwerpen, ontwikkelen en realiseren met tools zoals webdesign, animatie, AR/VR, videoproductie en programmeren. De focus ligt op innovatie, storytelling en technische vaardigheden, waardoor afgestudeerden klaar zijn voor een carrière in de creatieve en digitale sector.
+                        </p>
+                        <a href="https://www.erasmushogeschool.be/nl/opleidingen/multimedia-en-creatieve-technologie" target="_blank" rel="noopener noreferrer">Meer</a>
+                    </div>
+                        
+                </div>
             </section>
         </div>
     );
