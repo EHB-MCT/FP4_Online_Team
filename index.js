6ceca8feb1575f407cae5797c45bfb5719435384
Michiel-Janssen <157011760+MichielJanssenEHB@users.noreply.github.com>
2025-05-21 19:14:45 +0200

    header updates

--- index.js
+++ index.js
@@ -16,16 +16,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+	res.setHeader("Content-Security-Policy",
+		"default-src 'none'; " +
+		"img-src 'self'; " +
+		"style-src 'self' https://fonts.googleapis.com; " +
+		"font-src https://fonts.gstatic.com; " +
+		"script-src 'self'; " +
+		"connect-src 'self';"
+	);
+	next();
+});
+
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('/*\w', (req, res) => {
-    res.setHeader("Content-Security-Policy",
-        "default-src 'none'; " +
-        "img-src 'self'; " +
-        "style-src 'self' https://fonts.googleapis.com; " +
-        "font-src https://fonts.gstatic.com; " +
-        "script-src 'self'; "
-    );
     res.sendFile(path.join(__dirname, '/client/dist/index.html'));
 });
 
