05e4955a65cd145cc1f626e5a59af08dce78cbdb
Sam-Hoeterickx <125275911+Sam-Hoeterickx@users.noreply.github.com>
2025-05-29 12:33:22 +0200

    Updated countdown

--- client/src/components/CountDown.jsx
+++ client/src/components/CountDown.jsx
@@ -40,13 +40,20 @@ const CountDown = () => {
     return (
         <div className="countdown">
             
-            <div className="time-wrapper">
-                <h1>{timeLeft.months}</h1>
-                <p>Months</p>
-            </div>
-            <div>
-                <h1>:</h1>
-            </div>
+            { 
+                timeLeft.months !== "00" ? (
+                    <>
+                        <div className="time-wrapper">
+                            <h1>{timeLeft.months}</h1>
+                            <p>Months</p>
+                        </div>
+                                <div>
+                            <h1>:</h1>
+                        </div>
+                    </>
+                ) : null
+            }
+            
             <div className="time-wrapper">
                 <h1>{timeLeft.days}</h1>
                 <p>Days</p>
