2f639974c21f3eb9a4497ee6e9b4bd3cb11a88bd
Eren Sahbaz <eren.sahbaz@student.ehb.be>
2025-06-02 15:11:35 +0200

    Add Buttons component and update Projects page layout

--- client/src/modules/app/projects/page/Projects.page.jsx
+++ client/src/modules/app/projects/page/Projects.page.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import Search from "../components/Search";
-
+import Buttons from "../components/Buttons";
 export const Projects = () => {
 	const [searchTerm, setSearchTerm] = useState("");
 	return (
 		<>
 			<section>
 				<div className="inner-wrapper">
-					<h2 className="">Eindjaarprojecten</h2>
+					<h1 className="">Eindjaarprojecten</h1>
 					<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+					<Buttons />
 				</div>
 			</section>
 		</>
