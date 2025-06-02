12c5fda56cd821452228edc45f1d2652b64a0545
EliasGillot <elias.gillot@student.ehb.be>
2025-06-02 16:17:29 +0200

    Add complete structure for voting page with category selection and project cards

---
+++ client/src/modules/app/voting/page/Voting.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { CategorySelector } from "../components/CategorySelector";
+import { CategorySection } from "../components/CategorySection";
+
+export const Voting = () => {
+	document.title = "Voting | Expo 2025";
+
+	const [selected, setSelected] = useState(null);
+
+	const data = {
+		"App / Web": new Array(10).fill({
+			projectName: "Projectnaam",
+			name: "Naam",
+		}),
+		"Game / AR / VR": new Array(7).fill({
+			projectName: "Projectnaam",
+			name: "Naam",
+		}),
+		"Installatie / Prototyping": new Array(4).fill({
+			projectName: "Projectnaam",
+			name: "Naam",
+		}),
+		"Motion graphics": new Array(5).fill({
+			projectName: "Projectnaam",
+			name: "Naam",
+		}),
+	};
+
+	const handleSelect = (category) => {
+		if (selected === category) {
+			setSelected(null);
+		} else {
+			setSelected(category);
+		}
+	};
+
+	return (
+		<div>
+			<h1>Stem op jouw favoriete project</h1>
+			<CategorySelector selected={selected} onSelect={handleSelect} />
+
+			{}
+			{(selected ? [[selected, data[selected]]] : Object.entries(data)).map(
+				([category, projects]) => (
+					<CategorySection
+						key={category}
+						title={category}
+						projects={projects}
+					/>
+				)
+			)}
+		</div>
+	);
+};
