72a958c079207940f71f559415e7a4fe19507b35
IvanFernandezOrtiz <ivan.fernandez.ortiz@student.ehb.be>
2025-06-04 12:20:12 +0200

    WIP video popup

--- client/src/modules/shared/livestreamPopup/LivestreamPopup.jsx
+++ client/src/modules/shared/livestreamPopup/LivestreamPopup.jsx
@@ -1,8 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ReactPlayer from "react-player";
+import Draggable from "react-draggable";
+
+import "./livestreamPopup.css"
 
 export const LivestreamPopup = () => {
   const [showPopup, setShowPopup] = useState(false);
+  const nodeRef = useRef(null); // This avoids findDOMNode
 
   useEffect(() => {
     const timer = setTimeout(() => setShowPopup(true), 2000);
@@ -12,23 +16,31 @@ export const LivestreamPopup = () => {
   if (!showPopup) return null;
 
   return (
-    <div className="fixed bottom-4 right-4 bg-white shadow-xl rounded-2xl p-2 z-50 w-[300px]">
-      <div className="flex justify-between items-center mb-2">
-        <h2 className="text-sm font-semibold">Live Now</h2>
-        <button onClick={() => setShowPopup(false)} className="text-gray-500 hover:text-red-500">
-          ✖
-        </button>
+    <Draggable handle=".handle" nodeRef={nodeRef}>
+      <div
+        ref={nodeRef}
+        className="popup-container"
+      >
+        <div className= 'handle cursor-move popup-handle'>
+          {/* <h2 className="text-sm font-semibold">Live Now</h2> */}
+          <button
+            onClick={() => setShowPopup(false)}
+            className="popup-close"
+          >
+            ✖
+          </button>
+        </div>
+        <div className="popup-video">
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=L1Ta38LNcUE"
+            playing
+            muted
+            controls
+            width="100%"
+            height="100%"
+          />
+        </div>
       </div>
-      <div className="w-full h-[170px]">
-        <ReactPlayer
-          url="https://www.youtube.com/watch?v=L1Ta38LNcUE"
-          playing
-          controls
-          width="200PX"
-          height="100PX"
-          pip
-        />
-      </div>
-    </div>
+    </Draggable>
   );
 };
