d72ebe9b4784f7d4bf1041c5f77c07179eb4042f
Sam-Hoeterickx <125275911+Sam-Hoeterickx@users.noreply.github.com>
2025-06-05 08:54:56 +0200

    Fixed error message

--- client/src/modules/app/error/page/Error.page.jsx
+++ client/src/modules/app/error/page/Error.page.jsx
@@ -4,7 +4,7 @@ export const Error = () => {
             <section className="large-section">
                 <div className="inner-wrapper">
                     <div className="wrapper">
-                        <h3>Oeps this page doesnt excist</h3>
+                        <h3>Oeps deze pagina bestaat niet</h3>
                     </div>
                 </div>
             </section>
