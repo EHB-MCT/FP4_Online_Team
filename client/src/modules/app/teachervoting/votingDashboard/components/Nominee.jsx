1560e11b56d3329d4a081e2939e2ff0a10edc5f6
Sam-Hoeterickx <125275911+Sam-Hoeterickx@users.noreply.github.com>
2025-06-11 17:03:47 +0200

    Made Nominees dynamic

--- client/src/modules/app/teachervoting/votingDashboard/components/Nominee.jsx
+++ client/src/modules/app/teachervoting/votingDashboard/components/Nominee.jsx
@@ -3,11 +3,11 @@ import clsx from "clsx"
 //CSS
 import styles from "./nominee.module.scss"
 
-export const Nominee = () => {
+export const Nominee = ({ project_name }) => {
     return (
         <>
             <div className={clsx(styles["nominee-item"])}>
-                <h3>Naam</h3>
+                <h3>{ project_name }</h3>
             </div>
         </>
     )
--- client/src/modules/app/teachervoting/votingDashboard/pages/VotingDashboard.page.jsx
+++ client/src/modules/app/teachervoting/votingDashboard/pages/VotingDashboard.page.jsx
@@ -5,15 +5,34 @@ import { useEffect,useState } from "react";
 import styles from "./votingDashboard.module.scss"
 import { Nominee } from "../components/Nominee";
 
+
+
 export const VotingDashboard = () => {
+    const projects = {
+        "Impact prijs": [
+            { project_id: 1, project_name: "Project Alpha" },
+            { project_id: 2, project_name: "Project Beta" },
+            { project_id: 3, project_name: "Project Gamma" }
+        ],
+        "Jury prijs": [
+            { project_id: 4, project_name: "Project Delta" },
+            { project_id: 5, project_name: "Project Epsilon" },
+            { project_id: 6, project_name: "Project Zeta" }
+        ],
+        "Innovatie prijs": [
+            { project_id: 7, project_name: "Project Eta" },
+            { project_id: 8, project_name: "Project Theta" },
+            { project_id: 9, project_name: "Project Iota" }
+        ]
+    };
 
-    const [projects, setProjects] = useState([]);
+    // const [projects, setProjects] = useState([]);
 
-    useEffect(() => {
-        fetch(`https://api.shiftfestival.be/api/votes`)
-        .then( response => response.json())
-        .then( data => setProjects(data) );
-    }, [])
+    // useEffect(() => {
+    //     fetch(`https://api.shiftfestival.be/api/votes`)
+    //     .then( response => response.json())
+    //     .then( data => setProjects(data) );
+    // }, [])
 
     return (
         <>
@@ -25,8 +44,14 @@ export const VotingDashboard = () => {
                         <h3>Impactsprijs</h3>
                         <div className={clsx(styles["voting-dashboard-wrapper--category-wrapper--nominees-wrapper"])}>
                             
-                            {
-                                
+                            { 
+                                projects["Impact prijs"].map((element) => (
+                                    <Nominee 
+                                        key={element.project_id}
+                                        project_name={element.project_name}
+                                        project_id={element.project_id}
+                                    />
+                                ))
                             }
                             
                         </div>
@@ -35,24 +60,30 @@ export const VotingDashboard = () => {
                     <div className={clsx(styles["voting-dashboard-wrapper--category-wrapper"])}>
                         <h3>Jurypijs</h3>
                         <div className={clsx(styles["voting-dashboard-wrapper--category-wrapper--nominees-wrapper"])}>
-                            <Nominee />
-
-                            <Nominee />
-
-                            <Nominee />
-
+                            { 
+                                projects["Jury prijs"].map((element) => (
+                                    <Nominee 
+                                        key={element.project_id}
+                                        project_name={element.project_name}
+                                        project_id={element.project_id}
+                                    />
+                                ))
+                            }
                         </div>
                     </div>
 
                     <div className={clsx(styles["voting-dashboard-wrapper--category-wrapper"])}>
                         <h3>Innovatiepijs</h3>
                         <div className={clsx(styles["voting-dashboard-wrapper--category-wrapper--nominees-wrapper"])}>
-                            <Nominee />
-
-                            <Nominee />
-
-                            <Nominee />
-
+                            { 
+                                projects["Innovatie prijs"].map((element) => (
+                                    <Nominee 
+                                        key={element.project_id}
+                                        project_name={element.project_name}
+                                        project_id={element.project_id}
+                                    />
+                                ))
+                            }
                         </div>
                     </div>
                 </div>
