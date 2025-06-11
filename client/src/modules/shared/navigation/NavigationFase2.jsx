faab7d29cc580d737a9fd98ddb92b7bbc5407a34
IvanFernandezOrtiz <ivan.fernandez.ortiz@student.ehb.be>
2025-06-11 16:51:35 +0200

    nav fase 2 fix

--- client/src/modules/shared/navigation/NavigationFase2.jsx
+++ client/src/modules/shared/navigation/NavigationFase2.jsx
@@ -10,60 +10,62 @@ export const NavigationFase2 = () => {
 	return (
 		<nav>
 			<div className="inner-wrapper">
-				<Link to="/" className="home-link">
-					<img src={Image} alt="logo event" />
-				</Link>
+				<div className="nav-container">
+					<Link to="/" className="home-link">
+						<img src={Image} alt="logo event" />
+					</Link>
 
-				<ul className="nav-links">
-					<li>
-						<Link to="/" className="link">
-							Programma
-						</Link>
-					</li>
-
-					<li className="relative" onMouseEnter={() => setIsDropdownOpen(true)} onMouseLeave={() => setIsDropdownOpen(false)}>
-						<span className="link cursor-pointer">
-							<Link to="/projects" className="link">
-								Eindjaarprojecten ▾
+					<ul className="nav-links">
+						<li>
+							<Link to="/" className="link">
+								Programma
 							</Link>
-						</span>
-						{isDropdownOpen && (
-							<ul className="dropdown-menu">
-								<li>
-									<Link to="/projects/app-web" className="dropdown-link">
-										App/Web
-									</Link>
-								</li>
-								<li>
-									<Link to="/projects/game-vr" className="dropdown-link">
-										Game/AR/VR
-									</Link>
-								</li>
-								<li>
-									<Link to="/projects/installatie" className="dropdown-link">
-										Installatie/Prototype
-									</Link>
-								</li>
-								<li>
-									<Link to="/projects/motion" className="dropdown-link">
-										Motion Graphics
-									</Link>
-								</li>
-							</ul>
-						)}
-					</li>
+						</li>
 
-					<li>
-						<Link to="/price" className="link">
-							Prijzen
-						</Link>
-					</li>
-					<li>
-						<Link to="/info" className="link">
-							Info
-						</Link>
-					</li>
-				</ul>
+						<li className="relative" onMouseEnter={() => setIsDropdownOpen(true)} onMouseLeave={() => setIsDropdownOpen(false)}>
+							<span className="link cursor-pointer">
+								<Link to="/projects" className="link">
+									Eindjaarprojecten ▾
+								</Link>
+							</span>
+							{isDropdownOpen && (
+								<ul className="dropdown-menu">
+									<li>
+										<Link to="/projects/app-web" className="dropdown-link">
+											App/Web
+										</Link>
+									</li>
+									<li>
+										<Link to="/projects/game-vr" className="dropdown-link">
+											Game/AR/VR
+										</Link>
+									</li>
+									<li>
+										<Link to="/projects/installatie" className="dropdown-link">
+											Installatie/Prototype
+										</Link>
+									</li>
+									<li>
+										<Link to="/projects/motion" className="dropdown-link">
+											Motion Graphics
+										</Link>
+									</li>
+								</ul>
+							)}
+						</li>
+
+						<li>
+							<Link to="/price" className="link">
+								Prijzen
+							</Link>
+						</li>
+						<li>
+							<Link to="/info" className="link">
+								Info
+							</Link>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);
