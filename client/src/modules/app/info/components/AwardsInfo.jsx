2a7b2a0ced636c3cb4c306b32f34b16e7d6fe3b3
Sarah El Fraihi <sarah.el.fraihi@student.ehb.be>
2025-06-12 16:27:09 +0200

    img component awards info page weg gehaalt

--- client/src/modules/app/info/components/AwardsInfo.jsx
+++ client/src/modules/app/info/components/AwardsInfo.jsx
@@ -1,18 +1,22 @@
-import clsx from "clsx"
+import clsx from "clsx";
 
 //CSS
-import styles from "./awardsInfo.module.scss"
+import styles from "./awardsInfo.module.scss";
 
 export const AwardsInfo = ({ price_title, badge, description }) => {
-    return (
-        <div className={clsx(styles["award-info-wrapper"])}> 
-            <img className={clsx(styles["awards-info-wrapper--award-badge"])} src="Logo.svg" alt="" />
-            <h3 className="white-text" style={{textAlign: "start"}}>
-                { price_title }
-            </h3>
-            <p className="white-text" style={{textAlign: "start"}}>
-                { description }
-            </p>
-        </div>
-    )
-}
+	return (
+		<div className={clsx(styles["award-info-wrapper"])}>
+			<img
+				className={clsx(styles["awards-info-wrapper--award-badge"])}
+				src=""
+				alt=""
+			/>
+			<h3 className="white-text" style={{ textAlign: "start" }}>
+				{price_title}
+			</h3>
+			<p className="white-text" style={{ textAlign: "start" }}>
+				{description}
+			</p>
+		</div>
+	);
+};
